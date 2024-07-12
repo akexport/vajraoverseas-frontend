8835fa2df79d533627ae1cc48401508466fd4794
akexport <akexport23@gmail.com>
2024-07-12 17:15:42 +0530

    Contact page added

--- vajraoverseas-ui/src/components/HeaderComponent.jsx
+++ vajraoverseas-ui/src/components/HeaderComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const HeaderComponent = () => {
+  
   return (
     <div>
         <header>
@@ -14,8 +15,8 @@ const HeaderComponent = () => {
                   <input type="search" className='form-control' placeholder="Search..." aria-label="Search" />
                 </form> */}
                 <div className='container d-flex flex-wrap justify-content-left'>
-                  <a href="#" className='nav-link text-secondary'>
-                    <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlink:href="#home"></use></svg>
+                  <a href="/" className='nav-link text-secondary'>
+                    <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlinkHref="#logo"></use></svg>
                     <img className='logo' src='./Logo.jpg' />
                   </a>
                 </div>
@@ -26,44 +27,35 @@ const HeaderComponent = () => {
             {/* </div> */}
               <div className='container justify-content-end'>
                 <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start'>
-                  <a href="/" className='d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none'>
-                    <svg className='bi me-2' width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-                  </a>
                   <ul className='nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small'>
-                    {/* <li>
-                      <a href="#" className='nav-link text-secondary'>
-                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlink:href="#home"></use></svg>
-                        <img className='logo' src='./Logo.jpg' />
-                      </a>
-                    </li> */}
                     <li>
-                      <a href="#" className='nav-link text-secondary'>
-                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlink:href="#home"></use></svg>
+                      <a href="/" className='nav-link text-secondary'>
+                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlinkHref="#home"></use></svg>
                         Home
                       </a>
                     </li>
                     <li>
                       <a href="#" className='nav-link text-white'>
-                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlink:href="#speedometer2"></use></svg>
-                        Dashboard
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className='nav-link text-white'>
-                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlink:href="#table"></use></svg>
-                        Orders
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className='nav-link text-white'>
-                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlink:href="#grid"></use></svg>
+                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlinkHref="#grid"></use></svg>
                         Products
                       </a>
                     </li>
                     <li>
                       <a href="#" className='nav-link text-white'>
-                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlink:href="#people-circle"></use></svg>
+                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlinkHref="#people-circle"></use></svg>
                         Customers
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className='nav-link text-white'>
+                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlinkHref="#speedometer2"></use></svg>
+                        Services
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/add-customer" className='nav-link text-white'>
+                        <svg className='bi d-block mx-auto mb-1' width="24" height="24"><use xlinkHref="#table"></use></svg>
+                        Contact
                       </a>
                     </li>
                   </ul>

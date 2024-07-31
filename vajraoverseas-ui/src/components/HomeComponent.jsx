a07d5dd5632a8a2d058503cd91d8729695669d4f
akexport <akexport23@gmail.com>
2024-07-31 21:56:44 +0530

    added some content for home page

--- vajraoverseas-ui/src/components/HomeComponent.jsx
+++ vajraoverseas-ui/src/components/HomeComponent.jsx
@@ -8,7 +8,7 @@ function HomeComponent() {
                 <div className="container">
                 <div className="row">
                         <div className="col-sm"><div className="card" >
-                            <img src="./CheakPea1.jpg" className="card-img-top" alt="..." height="200px" />
+                            <img src="./CheakPea1.jpg" className="card-img-top" alt="..." height="500px" />
                         </div></div>
                         <div className="col-sm"><div className="card" >
                             <div className="card-body">
@@ -66,9 +66,15 @@ function HomeComponent() {
                         <div className="col-sm"><div className="card" >
                             <img src="./CheakPea4.jpg" className="card-img-top" alt="..." height="200px" />
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <h5 className="card-title">Interesting Facts</h5>
+                                <p className="card-text">
+                                <b>1. Nutritional Powerhouse:</b> Kabuli channa is a rich source of protein, dietary fiber, vitamins, and minerals such as iron, magnesium, and zinc. It is an excellent food for vegetarians and vegans due to its high protein content.<br />
+                                <b>2. Cultural Significance:</b> In India and the Middle East, Kabuli channa is not only a staple food but also holds cultural and religious significance. It is often prepared during festivals and special occasions.<br />
+                                <b>3. Storage and Preparation:</b> Dried Kabuli channa can be stored for an extended period in a cool, dry place. Before cooking, they should be soaked in water for several hours or overnight to reduce cooking time and improve digestibility.<br />
+                                <b>4. Environmental Impact:</b> Chickpeas are a sustainable crop as they improve soil health by fixing nitrogen, reducing the need for synthetic fertilizers. They also have a relatively low water footprint compared to other protein sources like meat.<br />
+                                <b>5. Economic Importance:</b> Kabuli channa is an important cash crop in many countries, contributing to the livelihoods of farmers and playing a crucial role in food security.<br />
+                                </p>
+                                {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
                             </div>
                         </div></div>
                         <div className="col-sm"><div className="card" >

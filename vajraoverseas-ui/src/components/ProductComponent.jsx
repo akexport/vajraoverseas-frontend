ca27a304973b08808a244546abc3c4d761a72d63
akexport <akexport23@gmail.com>
2025-04-03 20:45:20 +0530

    home page image update

--- vajraoverseas-ui/src/components/ProductComponent.jsx
+++ vajraoverseas-ui/src/components/ProductComponent.jsx
@@ -18,10 +18,11 @@ const ProductComponent = () => {
                     <div class="paper-bg">
                         <div class="section-title-furits text-center mb-80">
                             <img src="src/assets/HomepageFascinatinFacts.jpg" style={{ width: "100px", height: "100px" }} />
-                            <h5 class="section-title-furits">Vajra Overseas – Pioneering Global Trade with Power and Precision</h5>
+                            <br /><br />
+                            <h5 class="section-title-furits prod-headings-text">Vajra Overseas – Pioneering Global Trade with Power and Precision</h5>
                         </div>
                         <div className="row">
-                            <div className="col-sm fruits-choose-content">
+                            <div className="col-sm prod-headings-text">
                                 <p>
                                 “As the ‘land of spices’, India exports tons of various kinds of food. 
                                 In fact, food export from India is a major source of revenue. To help your 
@@ -34,20 +35,93 @@ const ProductComponent = () => {
                         </div>
                     </div>
                 </div>
-                <br />
-                <div className="container">    
+                <br /><br /><br />
+                <div className="container prod-grain-bg">
+                    <h4 className="card-title text-center prod-headings-text">GRAINS - A Powerhouse of Nutrition</h4>
+                    <p class="rounded float-end prod-headings-text">
+                        Kabuli Chana, also known as <b>Chickpeas, Chole Chana, or Garbanzo Beans,</b> is a nutrient-dense legume 
+                        packed with <b>protein, folic acid (Vitamin B9), magnesium, and dietary fiber.</b> It is highly beneficial 
+                        for <b>heart health, diabetes management, weight loss,</b> and overall well-being due to its <b>low glycemic index (GI).</b>
+                                            
+                    </p>
+                    <div className="row">
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                    </div>
+                </div>
+                <br /><br /><br />
+
+                <div className="container prod-sundry-bg">
+                    <h4 className="card-title text-center prod-headings-text">Sun-Dried Fruits & Vegetables – Preserving Nature’s Goodness</h4>
+                    <p class="rounded float-end">
+                        For generations, sun-drying has been a trusted method of preserving fruits and vegetables, ensuring a year-round supply of essential nutrients. 
+                        This traditional technique enhances flavors naturally, retaining the goodness of vitamins and minerals without the need for artificial preservatives.
+                        One such speciality is <b>Pahadi Badi,</b> made from <b>grated squash vegetables and lentil paste,</b> 
+                        carefully dried in <b>sun and shade.</b> These protein-rich nuggets are <b>mildly spiced or completely plain,</b> making them a perfect addition to a variety of dishes.
+                                
+                    </p>
+                    <div className="row">
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                    </div>
+                </div>
+                <br /><br /><br />
+
+                <div className="container prod-doiled-bg">
+                    <h4 className="card-title text-center prod-headings-text">De-Oiled Cakes - A Nutrient-Rich By-Product for Animal Feed & Fertilizer</h4>
+                    <p class="rounded float-start">
+                                
+                        <b>De-Oiled Cake (DOC)</b> is a valuable by-product obtained after extracting oil from various seeds through 
+                        mechanical pressing. Typically, <b>250–350 g of oil is extracted from 1 kg of oilseeds,</b> leaving behind 
+                        approximately <b>650 g of high-protein de-oiled cake</b> as residue. These seed cakes are widely used as 
+                        <b>nutrient-rich animal feed, organic fertilizers, and in aquaculture</b> due to their high protein, fiber, and essential nutrient content.
+                        
+                    </p>
+                    <div className="row">
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                    </div>
+                </div>
+                <br /><br /><br />
+
+                <div className="container prod-dryfruit-bg">
+                    <h4 className="card-title text-center prod-headings-text">Premium Dry Fruits - A Nutrient-Rich Superfood for a Healthy Life</h4>
+                    <p class="rounded float-start prod-headings-text">
+                        
+                        Dry fruits are nature’s powerhouse of <b>essential vitamins, minerals, proteins, and healthy fats,</b> 
+                        making them a must-have in a balanced diet. Recommended by health professionals, a handful of dry fruits 
+                        daily boosts <b>immunity, heart health, brain function, and energy levels.</b> Whether enjoyed as a snack, 
+                        added to desserts, or blended into smoothies, dry fruits provide <b>countless health benefits</b> when consumed 
+                        in moderation.
+                                
+                    </p>
+                    <div className="row">
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                        <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />
+                    </div>
+                </div>
+                <br /><br /><br />
+                {/* <div className="container">    
                     <div className="row">
                         <div className="col-sm"><div className="card">
                             <div className="card-body">
                             <img src="src/assets/Product_KabuliChana-02.png" class="rounded float-start" alt="..." style={{ width: "200px", height: "200px" }} />                         
                             
                                 <h4 className="card-title">Kabuli Chana – A Powerhouse of Nutrition</h4>
-                                {/* <p class="rounded float-start">
+                                <p class="rounded float-start">
                                 Kabuli Chana, also known as <b>Chickpeas, Chole Chana, or Garbanzo Beans,</b> is a nutrient-dense legume 
                                 packed with <b>protein, folic acid (Vitamin B9), magnesium, and dietary fiber.</b> It is highly beneficial 
                                 for <b>heart health, diabetes management, weight loss,</b> and overall well-being due to its <b>low glycemic index (GI).</b>
                                 <KabuliChannaPopupComponent />                     
-                            </p> */}
+                            </p>
                             </div>
                         </div></div>
                         <div className="col-sm"><div className="card" >
@@ -55,17 +129,17 @@ const ProductComponent = () => {
                                 
                             <img src="src/assets/Product_Sundried01.png" class="rounded float-end" alt="..." style={{ width: "200px", height: "150px" }} />
                             <h4 className="card-title">Sun-Dried Fruits & Vegetables – Preserving Nature’s Goodness</h4>
-                            {/* <p class="rounded float-end">
+                            <p class="rounded float-end">
                             For generations, sun-drying has been a trusted method of preserving fruits and vegetables, ensuring a year-round supply of essential nutrients. 
                             This traditional technique enhances flavors naturally, retaining the goodness of vitamins and minerals without the need for artificial preservatives.
                                 One such speciality is <b>Pahadi Badi,</b> made from <b>grated squash vegetables and lentil paste,</b> 
                                 carefully dried in <b>sun and shade.</b> These protein-rich nuggets are <b>mildly spiced or completely plain,</b> making them a perfect addition to a variety of dishes.
                                 <SunDriedPopupComponent />     
-                            </p> */}
+                            </p>
                             
                             </div>
                         </div></div>
-                    </div>    {/*row closed */}
+                    </div>   
                     <div className="row">
                         <div className="col-sm"><div className="card" >
                             <div className="card-body">
@@ -96,9 +170,9 @@ const ProductComponent = () => {
                             </p>
                             </div>
                         </div></div>
-                    </div>{/*row closed */}
+                    </div>
 
-                </div>{/*container closed */}
+                </div> */}
             </React.Fragment>
 
         </>
